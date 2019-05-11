a809eab93be9804e847baf446544d43a91b7cd25
Hector Camacho <danielcatu@gmail.com>
2019-05-10 21:44:30 -0500

    Mendi commit danger

--- I+D/DDOS.pl
+++ I+D/DDOS.pl
@@ -1,17 +1,16 @@
 #NOPOS{\ / : * ? " < > | }
 #! /usr/bin/perl
 $unidad='[a-zA-Z]';
-$Black='[^/][^\(\\\)]';
-$loco='[^(\\)/:\*?"<>\|]';
-$Definitiva='[\\\/\:\*\?\"\<\>\|]';
+$Black='[^(\\)/:\*?"<>\|]';
+$loco='[^(\\)/:\*?"<>\|][^\s]';
 print "Ingrese la dirección \n";
 my $dir=<stdin>;  
 print $dir;
-if($dir =~ /^$unidad\:((\\\"$Black+\"$)|(\\$loco+))*$/){
+if($dir =~ /^$unidad\:((\\\"$Black+\")|(\\$loco+))*$/){
       print "Reconocido\n";
 }else{
       print "NoReconocido\n";
 }  
 #c:\Windows\System32\Dri/vers\etc\hosts.pete
-#c:\Windows\System32\"Dri v/ers"\etc\hosts.pete
+#c:\Windows\System32\"Drive /rs"\etc\hosts.pete
 
